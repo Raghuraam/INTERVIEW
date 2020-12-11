4f6addb7b2998bd0d7f3f480efc8bfeff3efac5a
raghuraam <raghuraamcu@gmail.com>
2020-12-11 13:26:00 +0530

    removing unwanted spaces and formatting.

--- kata/src/main/java/com/incubyte/interview/kata/StringCalculator.java
+++ kata/src/main/java/com/incubyte/interview/kata/StringCalculator.java
@@ -4,31 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StringCalculator {
- 
+
 	private static int count;
-	
+
 	private static String escapedDelimiter(String str) {
 		String resultStr = "";
-		
-		for(int i=0; i<str.length(); i++) {
-			
-			if(str.charAt(i)=='|'){
-				resultStr = resultStr+str.charAt(i);
-			}else{
-				resultStr = resultStr+"\\"+str.charAt(i);	
+
+		for (int i = 0; i < str.length(); i++) {
+
+			if (str.charAt(i) == '|') {
+				resultStr = resultStr + str.charAt(i);
+			} else {
+				resultStr = resultStr + "\\" + str.charAt(i);
 			}
-				
-			
-			
+
 		}
 		return resultStr;
-		
 	}
 
 	public static int Add(String numbers) {
-		
+
 		count++;
-		
+
 		int total = 0;
 		if (numbers == null || numbers.isEmpty()) {
 			return 0;
@@ -36,69 +33,67 @@ public class StringCalculator {
 
 		String[] array = null;
 		List<String> demiliterList = new ArrayList<String>();
-		
-		if(numbers.charAt(0)=='['){
-			
-		array = numbers.split("\n");
-		String delimitersString = array[0];
-		
-		String temp = "";
-		for(int i=0; i<delimitersString.length(); i++){
-			
-			if(delimitersString.charAt(i)!='[' && delimitersString.charAt(i)!=']'){
-				temp = temp+delimitersString.charAt(i);
-			}else if(delimitersString.charAt(i)==']'){
-				demiliterList.add(temp.trim());
-				temp="";
+
+		if (numbers.charAt(0) == '[') {
+
+			array = numbers.split("\n");
+			String delimitersString = array[0];
+
+			String temp = "";
+			for (int i = 0; i < delimitersString.length(); i++) {
+
+				if (delimitersString.charAt(i) != '[' && delimitersString.charAt(i) != ']') {
+					temp = temp + delimitersString.charAt(i);
+				} else if (delimitersString.charAt(i) == ']') {
+					demiliterList.add(temp.trim());
+					temp = "";
+				}
 			}
-		}
-		
-		String escapedDelimiter = "";
-		for(String each: demiliterList){
-			
-			escapedDelimiter = escapedDelimiter+each+"|";
-			
-		}
-		
-		escapedDelimiter = escapedDelimiter.substring(0, escapedDelimiter.length()-1);
-		
-		array = array[1].split(escapedDelimiter(escapedDelimiter));
+
+			String escapedDelimiter = "";
+			for (String each : demiliterList) {
+
+				escapedDelimiter = escapedDelimiter + each + "|";
+
+			}
+
+			escapedDelimiter = escapedDelimiter.substring(0, escapedDelimiter.length() - 1);
+
+			array = array[1].split(escapedDelimiter(escapedDelimiter));
 
 		} else {
 			array = numbers.split(",|\\n|;");
 		}
 
 		for (String n : array) {
-			if(n.equals(" ") || n.isEmpty()){
+			if (n.equals(" ") || n.isEmpty()) {
 				continue;
 			}
-			
-				try {
-					int temp = Integer.parseInt(n);
-					if(temp<0){
-						throw new Exception("negatives not allowed "+n);	
-					}else if(temp>1000){
-						continue;
-					}
-					
-					total = total + temp;
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
+
+			try {
+				int temp = Integer.parseInt(n);
+				if (temp < 0) {
+					throw new Exception("negatives not allowed " + n);
+				} else if (temp > 1000) {
+					continue;
 				}
-			
+
+				total = total + temp;
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+
 		}
 
 		return total;
 	}
-	
-	public static int getCalledCount(){
-		
+
+	public static int getCalledCount() {
+
 		return count;
 	}
 
 	public static void main(String[] args) {
-		
-		
-	}
 
+	}
 }
--- kata/src/test/java/com/incubyte/interview/kata/StringCalculatorTest.java
+++ kata/src/test/java/com/incubyte/interview/kata/StringCalculatorTest.java
@@ -4,88 +4,87 @@ import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class StringCalculatorTest extends TestCase {
-	
+
 	/**
 	 * to test empty, "1", "1,2" inputs
 	 */
-	public void testCase1(){
-		
+	public void testCase1() {
+
 		Assert.assertEquals(0, StringCalculator.Add(""));
 		Assert.assertEquals(1, StringCalculator.Add("1"));
 		Assert.assertEquals(3, StringCalculator.Add("1,2"));
 	}
-	
+
 	/**
 	 * to test unknown amount of inputs
 	 */
-	public void testCase2(){
-		
+	public void testCase2() {
+
 		Assert.assertEquals(6, StringCalculator.Add("1,2,3"));
 		Assert.assertEquals(10, StringCalculator.Add("1,2,3,4"));
 		Assert.assertEquals(155, StringCalculator.Add("1,2,3,4,5,6,7,8,9,10,100"));
 	}
-	
+
 	/**
 	 * to test unknown amount of inputs
 	 */
-	public void testCase3(){
-		
+	public void testCase3() {
+
 		Assert.assertEquals(6, StringCalculator.Add("1\n2,3"));
-		
+
 	}
-	
+
 	/**
 	 * to support different delimiters
 	 */
-	public void testCase4(){
-		
+	public void testCase4() {
+
 		Assert.assertEquals(3, StringCalculator.Add(";\n1;2"));
 	}
-	
+
 	/**
 	 * to exclude negative numbers
 	 */
-	public void testCase5(){
+	public void testCase5() {
 		Assert.assertEquals(3, StringCalculator.Add(";\n1;2;-5"));
 	}
-	
+
 	/**
 	 * to exclude all negative numbers
 	 */
-	public void testCase6(){
+	public void testCase6() {
 		Assert.assertEquals(0, StringCalculator.Add(";\n-1;-2;-5"));
 	}
-	
-	public void testCase7(){
+
+	public void testCase7() {
 		Assert.assertEquals(6, StringCalculator.Add(";\n1;-2;-5;5;1005"));
 		Assert.assertNotSame(1005, StringCalculator.Add("1005"));
 	}
-	
+
 	/**
 	 * to get called count for Add method
 	 */
-	public void testCase8(){
-		
+	public void testCase8() {
+
 		Assert.assertNotSame(0, StringCalculator.getCalledCount());
 	}
-	
+
 	/**
 	 * to support longer length delimiter
 	 */
-	public void testCase9(){
+	public void testCase9() {
 		Assert.assertEquals(6, StringCalculator.Add("[***]\n1***2***3"));
 		Assert.assertEquals(106, StringCalculator.Add("[---]\n1---2---3---100"));
 	}
-	
+
 	/**
 	 * to handle multiple delimiters
 	 */
-	public void testCase10(){
+	public void testCase10() {
 		Assert.assertEquals(6, StringCalculator.Add("[**][%%]\n1**2%%3"));
 		Assert.assertEquals(6, StringCalculator.Add("[*][%]\n1*2%3"));
 		Assert.assertEquals(1000, StringCalculator.Add("[*][%]\n1000*2000%3000"));
 		Assert.assertEquals(0, StringCalculator.Add("[*][%]\n1001*2000%3000"));
 	}
-		
 
 }
